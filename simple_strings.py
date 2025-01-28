1ae30238228fb95ee5edab670adfe1dbdd6654ac
giuliannachireno <giuliannachireno@gmail.com>
2025-01-28 16:48:13 +0100

    adding "simple strings"

---
+++ simple_strings.py
@@ -0,0 +1,10 @@
+s1 = "John" # double quote
+s2 = 'Mary' # single quote
+
+print(s1)
+print(s2)
+
+s1 = "And then he asked: 'How are you'"
+print(s1)
+s1 ='And then he asked: "How are you?"'
+print(s1)
